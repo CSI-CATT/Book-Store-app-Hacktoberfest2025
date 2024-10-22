990cfef66bdf5d560cdf46cc41573a0489824be3
Aditya shankar jambhale <jambhaleaditya91@gmail.com>
2024-10-22 22:23:33 +0530

    Update signup.jsx

--- Frontend/src/components/signup.jsx
+++ Frontend/src/components/signup.jsx
@@ -18,7 +18,7 @@ function Signup() {
         };
 
         try {
-            const res = await axios.post("/user/signup", userinfo);
+            const res = await axios.post("https://book-store-app-lemon.vercel.app/user/signup", userinfo);
             console.log(res.data);
             if (res.data) {
                 toast.success('Signup Succesfully:)');
